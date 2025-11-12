ab0966dc4343792a00661fdada9243929561faf0
samwin25 <edorhsam@gmail.com>
2025-11-12 07:48:19 +0000

    fix: update AdminJS packages and fix babelHelpers runtime error

--- src/.adminjs/bundler.mjs
+++ src/.adminjs/bundler.mjs
@@ -1,7 +1,8 @@
-import { build } from '@adminjs/bundler';
+import { build } from '@adminjs/bundler'
 
 await build({
   entryPoint: './.adminjs/entry.js',
   destination: './.adminjs/bundle.js',
   babelHelpers: 'runtime',
-});
+  minify: false,
+})
